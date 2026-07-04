0c96f846169b0d28dd92c9c5a722c928b430c7ef
eiichikyo <redpandania@protonmail.com>
2026-07-04 23:21:12 +0800

    add new post

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,14 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
+import { defineCollection, z } from "astro:content";
 
-export const SITE_TITLE = 'Astro Blog';
-export const SITE_DESCRIPTION = 'Welcome to my website!';
+const blog = defineCollection({
+  schema: z.object({
+    title: z.string(),
+    date: z.date(),
+  }),
+});
+
+export const collections = {
+  blog,
+};

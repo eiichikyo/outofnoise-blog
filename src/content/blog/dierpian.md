5ed661c9d964ff2bf4aa2900fb785cc83dd4aec9
eiichikyo <redpandania@protonmail.com>
2026-07-05 19:25:18 +0800

    update homepage

--- src/content/blog/dierpian.md
+++
@@ -1,6 +0,0 @@
----
-title: ceshi
-description: 這是我的第一篇博客文章
-pubDate: 2026-07-05
----
-一個單純的測試。
--- src/content/config.ts
+++ src/content/config.ts
@@ -1,11 +1,11 @@
 import { defineCollection, z } from "astro:content";
 
-const blog = defineCollection({
-  schema: z.object({
-    title: z.string(),
-    description: z.string(),
-    pubDate: z.date(),
+export const collections = {
+  blog: defineCollection({
+    schema: z.object({
+      title: z.string(),
+      description: z.string(),
+      pubDate: z.coerce.date(),
+    }),
   }),
-});
-
-export const collections = { blog };
+};

1bb17da100114c555f5edcd2c5508e2bda2b3754
eiichikyo <redpandania@protonmail.com>
2026-07-13 04:48:37 +0800

    fix: 修正 content collection loader

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,7 +6,7 @@ import { defineConfig, fontProviders } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-	site: 'https://example.com',
+	site: 'site: 'https://outofnoise.netlify.app',',
 	integrations: [mdx(), sitemap()],
 	fonts: [
 		{
